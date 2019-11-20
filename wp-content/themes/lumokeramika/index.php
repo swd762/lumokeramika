091d9580354ac9cc02ce8175bd9b9b910b1cf415
swd762 <ilya01041984@gmail.com>
2019-11-21 01:18:20 +0400

    add main block

--- wp-content/themes/lumokeramika/index.php
+++ wp-content/themes/lumokeramika/index.php
@@ -2,14 +2,46 @@
     get_header();
 ?>
 
+<!--Main block-->
+    <main id="main" class="main">
+        <div class="main-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="hidden-xs col-sm-5">
+                        <div class="main-boxes">
+                            <img src="<?php echo get_template_directory_uri()?>/images/main/round.png" alt="Окружность" class="main-boxes-1 wow fadeInUp">
+                            <img src="<?php echo get_template_directory_uri()?>/images/main/boxes.png" alt="Коробки" class="main-boxes-2 wow fadeInLeft">
+                            <img src="<?php echo get_template_directory_uri()?>/images/main/symbol.png" alt="РСТ" class="main-boxes-3 wow fadeInRight">
+                        </div>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="main-form">
+                            <div class="main-form-wrapper">
+                                <div class="main-form-wrapper-header">
+                                    <h1>Продажа эксклюзивной плитки</h1>
+                                    <h1>светящейся в темноте</h1>
+                                    <p>с доставкой по всей России</p>
+                                </div>
+                                <form class="bid" action="mailer/smart.php" method="POST">
+                                    <p>Получите индивидуальное предложение на поставку светящейся плитки или мозики<br><span>Образцы бесплатно!</span></p>
+
+                                    <input id="phone_1" name="user_phone" type="tel" required placeholder="Введите номер телефона">
+                                    <button class="button" onclick="yaCounter47126406.reachGoal('bid-btn'); return true;" type="submit">Оставить заявку!</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
 
 
 
 
 </body>
-
-
-
 <?php
     get_footer();
 ?>
