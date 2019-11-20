38c2e5e21a563f563ea2d92abc20c9be0ea3f517
swd762 <ilya01041984@gmail.com>
2019-11-20 23:30:34 +0400

    Add adaptive header with menu

--- wp-content/themes/lumokeramika/functions.php
+++ wp-content/themes/lumokeramika/functions.php
@@ -1,6 +1,10 @@
 <?php
 
     //  ** Load bootstrap styles **
-    wp_enqueue_style('bootstrap-min', get_theme_file_uri('/bootstrap.min.css'));
+   wp_enqueue_style('bootstrap-min', get_theme_file_uri('/bootstrap.min.css'));
     // ** Load project common styles **
     wp_enqueue_style('style', get_theme_file_uri('/style.css'));
+    // ** Load bootsrap script **
+    wp_enqueue_script('bootstrap', get_theme_file_uri('/js/bootstrap.min.js'), array('jquery') );
+
+    wp_enqueue_script('jquery', get_theme_file_uri('/js/jquery-1.12.0.min.js') );
--- wp-content/themes/lumokeramika/header.php
+++ wp-content/themes/lumokeramika/header.php
@@ -1,14 +1,12 @@
-<?php
-// ** Header with menu **
-?>
 
 
 
-<!doctype html>
-<html <?php language_attributes(); ?>>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=1141">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11" />
     <title><?php echo wp_get_document_title();?></title>
 
@@ -31,7 +29,7 @@
                      <div class="col-sm-6 col-md-6">
                          <div class="wrap">
                              <div class="wrap-phone">
-                                 <p>8 (347) 271-54-28</p>
+                                 <a href="tel:+79272848574">+7 (927) 284-85-74</a>
                              </div>
                          </div>
                      </div>
@@ -48,49 +46,47 @@
                  </div>
              </div>
          </div>
-         <nav class="navbar">
+         <nav class="navbar navbar-default">
              <div class="container">
-                 <!-- Brand and toggle get grouped for better mobile display -->
-<!--                 <div class="navbar-default navbar-header">-->
-<!--                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">-->
-<!--                         <span class="sr-only">Toggle navigation</span>-->
-<!--                         <span class="icon-bar"></span>-->
-<!--                         <span class="icon-bar"></span>-->
-<!--                         <span class="icon-bar"></span>-->
-<!--                     </button>-->
-<!--                     <div class="navbar-header-call">-->
-<!--                         <div class="navbar-header-call-img">-->
-<!--                             <img src="img/header/logo-mobile.svg" alt="Логотип">-->
-<!--                         </div>-->
-<!--                         <div class="navbar-header-call-phones">-->
-<!--                             <p>8 (347) <span>271-54-28</span></p>-->
-<!--                             <p>8 (937) <span>363-30-00</span></p>-->
-<!--                         </div>-->
-<!--                         <div class="navbar-header-call-handset">-->
-<!--                             <div class="navbar-header-call-handset-img btn-order">-->
-<!--                                 <img src="img/header/call.svg" alt="Телефонная трубка">-->
-<!--                             </div>-->
-<!--                         </div>-->
-<!--                     </div>-->
-<!--                 </div>-->
-                 <!-- Collect the nav links, forms, and other content for toggling -->
-                     <div class="collapse navbar-collapse" id="bs-navbar-collapse">
-                         <ul class="nav navbar-nav">
-                             <li><a class="slowly" href="#main">О компании</a></li>
-                             <li><a class="slowly" href="#triggers">Преимущества</a></li>
-                             <li><a class="slowly" href="#make">Производство</a></li>
-                             <li><a class="slowly" href="#offer">Склад</a></li>
-                             <li><a class="slowly" href="#production">Продукция</a></li>
-                             <li><a class="slowly" href="#clients">Наши клиенты</a></li>
-                             <li><a class="slowly" href="#feedback">Сертификаты</a></li>
-                             <li><a class="slowly" href="#feedback">Отзывы</a></li>
-                             <li><a class="slowly" href="#contacts">Контакты</a></li>
-                         </ul>
+                 <!-- Toggler for mobile display -->
+                 <div class="navbar-default navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
+                         <span class="sr-only">Toggle navigation</span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                     </button>
+
+                  <!-- New header block for mobile version instead native -->
+                     <div class="navbar-header-call">
+                         <div class="navbar-header-call-img">
+                             <img src="<?php echo get_template_directory_uri()?>/images/favicon.png" alt="logo">
+                         </div>
+                         <div class="navbar-header-call-phones">
+                             <a href="tel:+79272848574">ООО "Люмокерамика"</a>
+                             <a href="tel:+79272848574">+7 (927) <span>284-85-74</span></a>
+                         </div>
+                         <div class="navbar-header-call-handset">
+                             <div class="navbar-header-call-handset-img btn-order">
+                                 <img src="<?php echo get_template_directory_uri()?>/images/call.svg" alt="Call">
+                             </div>
+                         </div>
                      </div>
+                 </div>
 
+                 <!-- Collect the nav links toggling -->
+                 <div class="collapse navbar-collapse" id="bs-navbar-collapse">
+                     <ul class="nav navbar-nav">
+                         <li><a class="slowly" href="#main">О компании</a></li>
+                         <li><a class="slowly" href="#triggers">Галерея</a></li>
+                         <li><a class="slowly" href="#make">Продукция</a></li>
+                         <li><a class="slowly" href="#production">Преимущества</a></li>
+                         <li><a class="slowly" href="#feedback">Сертификаты</a></li>
+                         <li><a class="slowly" href="#feedback">Отзывы</a></li>
+                         <li><a class="slowly" href="#feedback">Выбрать принт</a></li>
+                         <li><a class="slowly" href="#contacts">Контакты</a></li>
+                     </ul>
+                 </div>
              </div>
-
          </nav>
-
-
      </header>
