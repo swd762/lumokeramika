a4ffff1311b4781d6627f2d3f8bad8837f39f3e8
swd762 <ilya01041984@gmail.com>
2019-11-23 13:47:46 +0400

    add some blocks, add slick-slider gallery plugin, add some pictures in gallery

--- wp-content/themes/lumokeramika/functions.php
+++ wp-content/themes/lumokeramika/functions.php
@@ -1,10 +1,13 @@
 <?php
 
-    //  ** Load bootstrap styles **
-   wp_enqueue_style('bootstrap-min', get_theme_file_uri('/bootstrap.min.css'));
-    // ** Load project common styles **
+    //  ** Load styles **
+    wp_enqueue_style('bootstrap-css', get_theme_file_uri('/bootstrap.min.css'));
     wp_enqueue_style('style', get_theme_file_uri('/style.css'));
-    // ** Load bootsrap script **
-    wp_enqueue_script('bootstrap', get_theme_file_uri('/js/bootstrap.min.js'), array('jquery') );
+    wp_enqueue_style('slick', get_theme_file_uri('/addons/slick/slick.css'));
+    wp_enqueue_style('slick-theme', get_theme_file_uri('/addons/slick/slick-theme.css'));
 
+
+    // ** Load scripts **
     wp_enqueue_script('jquery', get_theme_file_uri('/js/jquery-1.12.0.min.js') );
+    wp_enqueue_script('bootstrap', get_theme_file_uri('/js/bootstrap.min.js'), array('jquery') );
+    wp_enqueue_script('slick', get_theme_file_uri('/addons/slick/slick.min.js'), array('jquery') );
--- wp-content/themes/lumokeramika/index.php
+++ wp-content/themes/lumokeramika/index.php
@@ -68,18 +68,202 @@
         </div>
     </section>
 <!--Gallery block-->
-    <section id="galley" class="gallry">
+    <section id="galley" class="gallery">
         <div class="gallery-overlay">
             <div class="container">
                 <div class="row">
+                    <div class="col-md-12">
+                        <div class="space"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="making-slider-for">
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/01.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/02.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/03.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/04.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/05.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/06.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/07.jpg" alt="Slider-img">
+                            </div>
+                            <div class="making-slider-for-block">
+                                <img src="<?php echo get_template_directory_uri()?>/images/slider/08.jpg" alt="Slider-img">
+                            </div>
+                        </div>
+                        <div class="slider-nav-wrapper">
+                            <div class="making-slider-nav hidden-xs wow fadeInUp" data-wow-delay="0.1s" data-wow-offset="150">
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/01.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/02.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/03.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/04.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/05.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/06.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/07.jpg" alt="Slider-img">
+                                </div>
+                                <div class="making-slider-nav-block">
+                                    <img src="<?php echo get_template_directory_uri()?>/images/slider/08.jpg" alt="Slider-img">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 hidden-xs hidden-sm">
+                        <div class="gallery-description">
+                            <div class="gallery-description-header">
+                                <h2>ГАЛЕРЕЯ ИНТЕРЬЕРОВ</h2>
+                            </div>
+                            <div class="gallery-description-text-block">
+                                <p>так наша плитка выглядит в интерьерах</p>
+                                <p>так наша плитка выглядит в интерьерах</p>
+                                <p>так наша плитка выглядит в интерьерах</p>
+                                <p>так наша плитка выглядит в интерьерах</p>
+                                <p>так наша плитка выглядит в интерьерах</p>
 
+                            </div>
+                        </div>
 
+                    </div>
+                        <!--                        <div class="gallery-view-box">-->
+<!---->
+<!--                        </div>-->
+<!--                        <div class="gallery-thumbnails-box">-->
+<!---->
+<!--                        </div>-->
+<!--
+                    </div>
+                    <div class="hidden-xs hidden-sm col-md-6">-->
+<!--                        <div class="gallery-description">-->
+<!---->
+<!--                        </div>-->
+<!--                    </div>-->
                 </div>
             </div>
         </div>
     </section>
+    <section id="products" class="products">
+        <div class="container">
+            <div class="products-header">
+                <h2>НАША ПРОДУКЦИЯ</h2>
+            </div>
+            <div class="row">
+                 <div class="col-sm-6 col-xs-12">
+                     <div class="products-content">
+                         <img src="" alt="">
+                         <h4>Пластина настенная</h4>
+                     </div>
+                 </div>
 
 
+
+
+
+
+
+            </div>
+        </div>
+    </section>
+<script>
+       jQuery(function($) {
+        /*Slick Script*/
+        $('.making-slider-for').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: true,
+            asNavFor: '.making-slider-nav',
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        prevArrow: '<button class="making-arrow-prev"></button>',
+                        nextArrow: '<button class="making-arrow-next"></button>',
+                    }
+                },
+            ]
+        });
+
+        $('.making-slider-nav').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '<button class="making-arrow-prev"></button>',
+            nextArrow: '<button class="making-arrow-next"></button>',
+            asNavFor: '.making-slider-for',
+            dots: false,
+            arrows: true,
+            centerMode: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+            ]
+        });
+
+
+        $('.feedback-slider-nav').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: '<button class="prev arrow"></button>',
+            nextArrow: '<button class="next arrow"></button>',
+            arrows: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        prevArrow: '<button class="making-arrow-prev"></button>',
+                        nextArrow: '<button class="making-arrow-next"></button>',
+                    }
+                },
+            ]
+        });
+    });
+</script>
 </body>
 <?php
     get_footer();
