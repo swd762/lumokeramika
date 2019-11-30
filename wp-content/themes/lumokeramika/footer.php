8e5923ad9237af01a5e0bf52b76530bc1213bc08
swd762 <ilya01041984@gmail.com>
2019-11-30 13:49:16 +0400

    add yandex.metrika in footer

--- wp-content/themes/lumokeramika/footer.php
+++ wp-content/themes/lumokeramika/footer.php
@@ -67,6 +67,26 @@
                 </div>
             </div>
         </div>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(56462173, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/56462173" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
+
+
+
 <script>
     jQuery(function($){
         $("#phone_1").mask("+7 (999) 999-99-99");
